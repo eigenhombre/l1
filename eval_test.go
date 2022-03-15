3da32a6a6cb24e40bd78c981b3c4cdc610fa6c63
John Jacobsen <eigenhombre@gmail.com>
2022-03-14 21:32:29 -0500

    Add possibility of testing errors from Eval.  For #15.

--- eval_test.go
+++ eval_test.go
@@ -1,135 +1,137 @@
 package main
 
 import (
-	"fmt"
+	"strings"
 	"testing"
 )
 
 func TestEval(t *testing.T) {
-	var tests = []struct {
+	type evalCase struct {
 		in  string
 		out string
-	}{
-		// The first several tests are output in a format that is easy to copy
-		// into the README.  They should be some of the more "interesting" ones
-		// to illustrate the state of the language.  Tricky edge cases less
-		// interesting to the reader occur later in the list.
-		{"t", "t"},
-		{"()", "()"},
-		{"(cons t ())", "(t)"},
-		{"(cons (quote hello) (quote (world)))", "(hello world)"},
-		{"(quote foo)", "foo"},
-		{
-			"(quote (the (ten (laws (of (greenspun))))))",
-			"(the (ten (laws (of (greenspun)))))",
-		},
-		{"(cdr (quote (is not common lisp)))", "(not common lisp)"},
-		{"(car (quote (is not common lisp)))", "is"},
-		{"(cond (() 1) (2 3))", "3"},
-		{"1", "1"},
-		{"-5", "-5"},
-		{"(* 12349807213490872130987 12349807213490872130987)",
-			"152517738210391179737088822267441718485594169"},
-		{"(+)", "0"},
-		{"(+ 1 1 2 3)", "7"},
-		// {"(-)", "ERROR"},
-		{"(+ 1 1)", "2"},
-		{"(eq (quote foo) (quote foo))", "t"},
-		{"(eq (quote foo) (quote bar))", "()"},
-		{"(eq (quote foo) (quote (foo bar)))", "()"},
-		{"(atom (quote foo))", "t"},
-		{"(atom (quote (foo bar)))", "()"},
-		{"(+ 1)", "1"},
-		{"(+ -1)", "-1"},
-		{"(+ 0)", "0"},
-		{"(+ 1 2 3 4 5 6 7 8 9 10)", "55"},
-		{"(+ 999999999999999 1)", "1000000000000000"},
-		{"(+ 1 999999999999999)", "1000000000000000"},
-		{"(+ (+ 1))", "1"},
-		{"(+ (+ 1 2 3) 4 5 6)", "21"},
-		{"(- 1)", "-1"},
-		{"(- 1 1)", "0"},
-		{
-			"31489071430987532109487513094875031984750983147",
-			"31489071430987532109487513094875031984750983147",
-		},
-		{"(- 12349807213490872130987 12349807213490872130987)", "0"},
-		{"(- (+ 1 2 3) 4 5 6)", "-9"},
-		{"(*)", "1"},
-		{"(* 1 1)", "1"},
-		{"(* 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20)", "2432902008176640000"},
-		{"(/ 1 1)", "1"},
-		// {"(/)", "ERROR"},
-		{"(/ 4 2)", "2"},
-		{"(/ 1 2)", "0"},
-		// `a` is bound in the test environment:
-		{"a", "1"},
-		{"(* a a a (*) (*) (*))", "1"},
-		{"(+ a a a (+) (+) (+))", "3"},
-		{"(quote 3)", "3"},
-		{"(quote (1 2 3))", "(1 2 3)"},
-		{"(quote ())", "()"},
-		{"(quote (((1 2 3))))", "(((1 2 3)))"},
-		{"(car (quote (1 2 3)))", "1"},
-		{"(cdr (quote (1 2 3)))", "(2 3)"},
-		{"(cons 1 (quote (2 3 4)))", "(1 2 3 4)"},
-		{"(cons (cons 1 (cons 2 ())) (quote (3 4)))", "((1 2) 3 4)"},
-		{"(quote ((1)))", "((1))"},
-		{"(quote (()))", "(())"},
-		{"(quote (() ()))", "(() ())"},
-		{"(cons () ())", "(())"},
-		// // {"(cons)", "ERROR"},
-		{"(eq (quote (foo bar)) (quote (foo bar)))", "()"},
-		{"(cond)", "()"},
-		{"(cond (() 3))", "()"},
-		{"(cond (3 3))", "3"},
-		{"(cond (() 3) (() 4))", "()"},
-		{"(cond (t 3) (t 4))", "3"},
+		err string
 	}
-	for i, test := range tests {
-		got, err := lexAndParse(test.in)
-		if err != nil {
-			t.Errorf("lexAndParse(%q) failed: %v", test.in, err)
-		}
-		testEnv := env{"a": Num(1)}
-		res := got[0].Eval(&testEnv)
-		if res.String() != test.out {
-			t.Errorf("eval(%q) = %q, want %q", test.in, res, test.out)
-		} else {
-			if i > 16 {
-				t.Logf("eval(%q) = %q", test.in, res)
-			} else {
-				// Print first few formatted for README
-				fmt.Printf("    > %s\n    %s\n", test.in, res)
-			}
-		}
+	S := func(in, out, err string) evalCase {
+		return evalCase{in, out, err}
 	}
-}
-
-func TestDef(t *testing.T) {
+	Cases := func(cases ...evalCase) []evalCase {
+		return cases
+	}
+	OK := "" // No error reported
 	var tests = []struct {
-		in  string
-		out string
+		evalCases []evalCase
 	}{
-		{"(def a 1)", "1"},
-		{"(def a (quote (1 2 3)))", "(1 2 3)"},
-		{"(def a t)", "t"},
-		{"(def a (+ 1 1))", "2"},
-		{"(def a ())", "()"},
+		{Cases(S("t", "t", OK))},
+		{Cases(S("()", "()", OK))},
+		{Cases(S("(cons t ())", "(t)", OK))},
+		{Cases(S("(cons (quote hello) (quote (world)))", "(hello world)", OK))},
+		{Cases(S("(quote foo)", "foo", OK))},
+		{Cases(S("(quote (the (ten (laws (of (greenspun))))))", "(the (ten (laws (of (greenspun)))))", OK))},
+		{Cases(S("(cdr (quote (is not common lisp)))", "(not common lisp)", OK))},
+		{Cases(S("(car (quote (is not common lisp)))", "is", OK))},
+		{Cases(S("(cond (() 1) (2 3))", "3", OK))},
+		{Cases(S("1", "1", OK))},
+		{Cases(S("-5", "-5", OK))},
+		{Cases(S("(* 12349807213490872130987 12349807213490872130987)", "152517738210391179737088822267441718485594169", OK))},
+		{Cases(S("(+)", "0", OK))},
+		{Cases(S("(+ 1 1 2 3)", "7", OK))},
+		{Cases(S("(+ 1 1)", "2", OK))},
+		{Cases(S("(eq (quote foo) (quote foo))", "t", OK))},
+		{Cases(S("(eq (quote foo) (quote bar))", "()", OK))},
+		{Cases(S("(eq (quote foo) (quote (foo bar)))", "()", OK))},
+		{Cases(S("(atom (quote foo))", "t", OK))},
+		{Cases(S("(atom (quote (foo bar)))", "()", OK))},
+		{Cases(S("(+ 1)", "1", OK))},
+		{Cases(S("(+ -1)", "-1", OK))},
+		{Cases(S("(+ 0)", "0", OK))},
+		{Cases(S("(+ 1 2 3 4 5 6 7 8 9 10)", "55", OK))},
+		{Cases(S("(+ 999999999999999 1)", "1000000000000000", OK))},
+		{Cases(S("(+ 1 999999999999999)", "1000000000000000", OK))},
+		{Cases(S("(+ (+ 1))", "1", OK))},
+		{Cases(S("(+ (+ 1 2 3) 4 5 6)", "21", OK))},
+		{Cases(S("(- 1)", "-1", OK))},
+		{Cases(S("(- 1 1)", "0", OK))},
+		{Cases(S("31489071430987532109487513094875031984750983147", "31489071430987532109487513094875031984750983147", OK))},
+		{Cases(S("(- 12349807213490872130987 12349807213490872130987)", "0", OK))},
+		{Cases(S("(- (+ 1 2 3) 4 5 6)", "-9", OK))},
+		{Cases(S("(*)", "1", OK))},
+		{Cases(S("(* 1 1)", "1", OK))},
+		{Cases(S("(* 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20)", "2432902008176640000", OK))},
+		{Cases(S("(/ 1 1)", "1", OK))},
+		// Future test cases w/ errors:
+		// (/ 1 0)
+		// (/)
+		// (-)
+		// (cons)
+		{Cases(S("(/ 4 2)", "2", OK))},
+		{Cases(S("(/ 1 2)", "0", OK))},
+		{Cases(S("(* 1 1 1 (*) (*) (*))", "1", OK))},
+		{Cases(S("(+ 1 1 1 (+) (+) (+))", "3", OK))},
+		{Cases(S("(quote 3)", "3", OK))},
+		{Cases(S("(quote (1 2 3))", "(1 2 3)", OK))},
+		{Cases(S("(quote ())", "()", OK))},
+		{Cases(S("(quote (((1 2 3))))", "(((1 2 3)))", OK))},
+		{Cases(S("(car (quote (1 2 3)))", "1", OK))},
+		{Cases(S("(cdr (quote (1 2 3)))", "(2 3)", OK))},
+		{Cases(S("(cons 1 (quote (2 3 4)))", "(1 2 3 4)", OK))},
+		{Cases(S("(cons (cons 1 (cons 2 ())) (quote (3 4)))", "((1 2) 3 4)", OK))},
+		{Cases(S("(quote ((1)))", "((1))", OK))},
+		{Cases(S("(quote (()))", "(())", OK))},
+		{Cases(S("(quote (() ()))", "(() ())", OK))},
+		{Cases(S("(cons () ())", "(())", OK))},
+		{Cases(S("(eq (quote (foo bar)) (quote (foo bar)))", "()", OK))},
+		{Cases(S("(cond)", "()", OK))},
+		{Cases(S("(cond (() 3))", "()", OK))},
+		{Cases(S("(cond (3 3))", "3", OK))},
+		{Cases(S("(cond (() 3) (() 4))", "()", OK))},
+		{Cases(S("(cond (t 3) (t 4))", "3", OK))},
+		// Whitespace cases
+		{Cases(S(" t ", "t", OK))},
+		{Cases(S("t\n", "t", OK))},
+		{Cases(S("\n\nt\n", "t", OK))},
+		// Multiple statements
+		{Cases(
+			S("1", "1", OK),
+			S("2", "2", OK),
+			S("3", "3", OK))},
+		// Global scope
+		{Cases(
+			S("(def a 3)", "3", OK),
+			S("a", "3", OK))},
+		{Cases(
+			S("(def a 3)", "3", OK),
+			S("(def a 4)", "4", OK),
+			S("a", "4", OK))},
+		{Cases(
+			S("(def a 6)", "6", OK),
+			S("(def b 7)", "7", OK),
+			S("(+ a b)", "13", OK))},
+		{Cases(
+			S("(def l (quote (1 2 3)))", "(1 2 3)", OK),
+			S("l", "(1 2 3)", OK))},
+		{Cases(
+			S("(def a (+ 1 1))", "2", OK),
+			S("(def b (+ a a))", "4", OK),
+			S("b", "4", OK))},
 	}
 	for _, test := range tests {
-		got, err := lexAndParse(test.in)
-		if err != nil {
-			t.Errorf("lexAndParse(%q) failed: %v", test.in, err)
-		}
-		testEnv := env{}
-		res := got[0].Eval(&testEnv)
-		if res.String() != test.out {
-			t.Errorf("eval(%q) = %q, want %q", test.in, res, test.out)
-		}
-		fetched := testEnv["a"]
-		if fetched.String() != test.out {
-			t.Errorf("testEnv[\"a\"] = %q, want %q", fetched, test.out)
+		e := env{}
+		for _, testCase := range test.evalCases {
+			got, err := lexAndParse(testCase.in)
+			if len(got) != 1 {
+				t.Errorf("%s: got %d results, want 1", testCase.in, len(got))
+				continue
+			}
+			if err != nil && !strings.Contains(err.Error(), testCase.err) {
+				t.Errorf("%s: got error %q, want %q", testCase.in, err, testCase.err)
+				continue
+			}
+			result := got[0].Eval(&e).String()
+			if result != testCase.out {
+				t.Errorf("%s: got %q, want %q", testCase.in, result, testCase.out)
+				continue
+			}
+			t.Logf("%s -> %q", testCase.in, result)
 		}
 	}
 }
