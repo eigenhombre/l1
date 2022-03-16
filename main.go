a6bb176113390eaaf2954b720ab184503fe5bfff
John Jacobsen <eigenhombre@gmail.com>
2022-03-16 18:51:27 -0500

    Execute files!!!!  Fixes #14.

--- main.go
+++ main.go
@@ -19,27 +19,28 @@ func readLine() (string, error) {
 	}
 }
 
-func main() {
-	globals := env{}
+func lexParseEvalPrint(s string, e env) {
+	got, err := lexAndParse(s)
+	if err != nil {
+		fmt.Printf("%v\n", err)
+		return
+	}
+	for _, g := range got {
+		_, err := g.Eval(&e)
+		if err != nil {
+			fmt.Printf("%v\n", err)
+			continue
+		}
+	}
+}
+
+func repl(e env) {
 	for {
 		fmt.Print("> ")
 		s, err := readLine()
 		switch err {
 		case nil:
-			got, err := lexAndParse(s)
-			if err != nil {
-				fmt.Printf("%v\n", err)
-				continue
-			}
-			if len(got) == 0 {
-				continue
-			}
-			ev, err := got[0].Eval(&globals)
-			if err != nil {
-				fmt.Printf("%v\n", err)
-				continue
-			}
-			fmt.Println(ev) // fixme: handle multiple items
+			lexParseEvalPrint(s, e)
 		case io.EOF:
 			fmt.Println()
 			return
@@ -47,4 +48,17 @@ func main() {
 			panic(err)
 		}
 	}
+}
+
+func main() {
+	globals := env{}
+	if len(os.Args) > 1 {
+		bytes, err := os.ReadFile(os.Args[1])
+		if err != nil {
+			panic(err)
+		}
+		lexParseEvalPrint(string(bytes), globals)
+		return
+	}
+	repl(globals)
 }
