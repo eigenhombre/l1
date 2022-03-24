f90f2881f0a16f980d135b73c8d8de7f69a962ef
John Jacobsen <eigenhombre@gmail.com>
2022-03-23 21:24:15 -0500

    Add (somewhat provisional) help function.

--- builtin.go
+++ builtin.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sort"
 	"strings"
 )
 
@@ -29,213 +30,238 @@ func (b Builtin) Equal(o Sexpr) bool {
 	return false
 }
 
-var builtins = map[string]*Builtin{
-	"+": {
-		Name: "+",
-		Fn: func(args []Sexpr) (Sexpr, error) {
-			if len(args) == 0 {
-				return Num(0), nil
-			}
-			sum := Num(0)
-			for _, arg := range args {
-				n, ok := arg.(Number)
+func doHelp() {
+	fmt.Println("Builtins and Special Forms:")
+	forms := []string{"def", "lambda", "cond", "quote"}
+	for _, builtin := range builtins {
+		forms = append(forms, builtin.Name)
+	}
+	// sort forms
+	sort.Strings(forms)
+	for _, form := range forms {
+		fmt.Printf("  %s\n", form)
+	}
+}
+
+// moving `builtins` into `init` avoids initialization loop for doHelp:
+var builtins map[string]*Builtin
+
+func init() {
+	builtins = map[string]*Builtin{
+		"+": {
+			Name: "+",
+			Fn: func(args []Sexpr) (Sexpr, error) {
+				if len(args) == 0 {
+					return Num(0), nil
+				}
+				sum := Num(0)
+				for _, arg := range args {
+					n, ok := arg.(Number)
+					if !ok {
+						return nil, fmt.Errorf("expected number, got '%s'", arg)
+					}
+					sum = sum.Add(n)
+				}
+				return sum, nil
+			},
+		},
+		"-": {
+			Name: "-",
+			Fn: func(args []Sexpr) (Sexpr, error) {
+				if len(args) == 0 {
+					return nil, fmt.Errorf("missing argument")
+				}
+				sum, ok := args[0].(Number)
 				if !ok {
-					return nil, fmt.Errorf("expected number, got '%s'", arg)
+					return nil, fmt.Errorf("expected number, got '%s'", args[0])
 				}
-				sum = sum.Add(n)
-			}
-			return sum, nil
+				if len(args) == 1 {
+					return args[0].(Number).Neg(), nil
+				}
+				for _, arg := range args[1:] {
+					n, ok := arg.(Number)
+					if !ok {
+						return nil, fmt.Errorf("expected number, got '%s'", arg)
+					}
+					sum = sum.Sub(n)
+				}
+				return sum, nil
+			},
 		},
-	},
-	"-": {
-		Name: "-",
-		Fn: func(args []Sexpr) (Sexpr, error) {
-			if len(args) == 0 {
-				return nil, fmt.Errorf("missing argument")
-			}
-			sum, ok := args[0].(Number)
-			if !ok {
-				return nil, fmt.Errorf("expected number, got '%s'", args[0])
-			}
-			if len(args) == 1 {
-				return args[0].(Number).Neg(), nil
-			}
-			for _, arg := range args[1:] {
-				n, ok := arg.(Number)
+		"*": {
+			Name: "*",
+			Fn: func(args []Sexpr) (Sexpr, error) {
+				if len(args) == 0 {
+					return Num(1), nil
+				}
+				prod := Num(1)
+				for _, arg := range args {
+					n, ok := arg.(Number)
+					if !ok {
+						return nil, fmt.Errorf("expected number, got '%s'", arg)
+					}
+					prod = prod.Mul(n)
+				}
+				return prod, nil
+			},
+		},
+		"/": {
+			Name: "/",
+			Fn: func(args []Sexpr) (Sexpr, error) {
+				if len(args) < 2 {
+					return nil, fmt.Errorf("missing argument")
+				}
+				quot, ok := args[0].(Number)
 				if !ok {
-					return nil, fmt.Errorf("expected number, got '%s'", arg)
+					return nil, fmt.Errorf("expected number, got '%s'", args[0])
 				}
-				sum = sum.Sub(n)
-			}
-			return sum, nil
+				for _, arg := range args[1:] {
+					if arg.Equal(Num(0)) {
+						return nil, fmt.Errorf("division by zero")
+					}
+					n, ok := arg.(Number)
+					if !ok {
+						return nil, fmt.Errorf("expected number, got '%s'", arg)
+					}
+					quot = quot.Div(n)
+				}
+				return quot, nil
+			},
 		},
-	},
-	"*": {
-		Name: "*",
-		Fn: func(args []Sexpr) (Sexpr, error) {
-			if len(args) == 0 {
-				return Num(1), nil
-			}
-			prod := Num(1)
-			for _, arg := range args {
-				n, ok := arg.(Number)
+		"car": {
+			Name: "car",
+			Fn: func(args []Sexpr) (Sexpr, error) {
+				if len(args) != 1 {
+					return nil, fmt.Errorf("missing argument")
+				}
+				carCons, ok := args[0].(*ConsCell)
 				if !ok {
-					return nil, fmt.Errorf("expected number, got '%s'", arg)
+					return nil, fmt.Errorf("'%s' is not a list", args[0])
 				}
-				prod = prod.Mul(n)
-			}
-			return prod, nil
+				return carCons.car, nil
+			},
 		},
-	},
-	"/": {
-		Name: "/",
-		Fn: func(args []Sexpr) (Sexpr, error) {
-			if len(args) < 2 {
-				return nil, fmt.Errorf("missing argument")
-			}
-			quot, ok := args[0].(Number)
-			if !ok {
-				return nil, fmt.Errorf("expected number, got '%s'", args[0])
-			}
-			for _, arg := range args[1:] {
-				if arg.Equal(Num(0)) {
-					return nil, fmt.Errorf("division by zero")
+		"cdr": {
+			Name: "cdr",
+			Fn: func(args []Sexpr) (Sexpr, error) {
+				if len(args) != 1 {
+					return nil, fmt.Errorf("missing argument")
 				}
-				n, ok := arg.(Number)
+				cdrCons, ok := args[0].(*ConsCell)
 				if !ok {
-					return nil, fmt.Errorf("expected number, got '%s'", arg)
+					return nil, fmt.Errorf("'%s' is not a list", args[0])
 				}
-				quot = quot.Div(n)
-			}
-			return quot, nil
+				return cdrCons.cdr, nil
+			},
 		},
-	},
-	"car": {
-		Name: "car",
-		Fn: func(args []Sexpr) (Sexpr, error) {
-			if len(args) != 1 {
-				return nil, fmt.Errorf("missing argument")
-			}
-			carCons, ok := args[0].(*ConsCell)
-			if !ok {
-				return nil, fmt.Errorf("'%s' is not a list", args[0])
-			}
-			return carCons.car, nil
+		"cons": {
+			Name: "cons",
+			Fn: func(args []Sexpr) (Sexpr, error) {
+				if len(args) != 2 {
+					return nil, fmt.Errorf("missing argument")
+				}
+				return Cons(args[0], args[1].(*ConsCell)), nil
+			},
 		},
-	},
-	"cdr": {
-		Name: "cdr",
-		Fn: func(args []Sexpr) (Sexpr, error) {
-			if len(args) != 1 {
-				return nil, fmt.Errorf("missing argument")
-			}
-			cdrCons, ok := args[0].(*ConsCell)
-			if !ok {
-				return nil, fmt.Errorf("'%s' is not a list", args[0])
-			}
-			return cdrCons.cdr, nil
+		"atom": {
+			Name: "atom",
+			Fn: func(args []Sexpr) (Sexpr, error) {
+				if len(args) != 1 {
+					return nil, fmt.Errorf("missing argument")
+				}
+				_, ok := args[0].(Atom)
+				if ok {
+					return Atom{"t"}, nil
+				}
+				return Nil, nil
+			},
 		},
-	},
-	"cons": {
-		Name: "cons",
-		Fn: func(args []Sexpr) (Sexpr, error) {
-			if len(args) != 2 {
-				return nil, fmt.Errorf("missing argument")
-			}
-			return Cons(args[0], args[1].(*ConsCell)), nil
-		},
-	},
-	"atom": {
-		Name: "atom",
-		Fn: func(args []Sexpr) (Sexpr, error) {
-			if len(args) != 1 {
-				return nil, fmt.Errorf("missing argument")
-			}
-			_, ok := args[0].(Atom)
-			if ok {
+		"eq": {
+			Name: "eq",
+			Fn: func(args []Sexpr) (Sexpr, error) {
+				if len(args) < 1 {
+					return nil, fmt.Errorf("missing argument")
+				}
+				for _, arg := range args[1:] {
+					if !args[0].Equal(arg) {
+						return Nil, nil
+					}
+				}
 				return Atom{"t"}, nil
-			}
-			return Nil, nil
+			},
 		},
-	},
-	"eq": {
-		Name: "eq",
-		Fn: func(args []Sexpr) (Sexpr, error) {
-			if len(args) < 1 {
-				return nil, fmt.Errorf("missing argument")
-			}
-			for _, arg := range args[1:] {
-				if !args[0].Equal(arg) {
-					return Nil, nil
+		"print": {
+			Name: "print",
+			Fn: func(args []Sexpr) (Sexpr, error) {
+				strArgs := []string{}
+				for _, arg := range args {
+					strArgs = append(strArgs, arg.String())
 				}
-			}
-			return Atom{"t"}, nil
+				fmt.Println(strings.Join(strArgs, " "))
+				return Nil, nil
+			},
 		},
-	},
-	"print": {
-		Name: "print",
-		Fn: func(args []Sexpr) (Sexpr, error) {
-			strArgs := []string{}
-			for _, arg := range args {
-				strArgs = append(strArgs, arg.String())
-			}
-			fmt.Println(strings.Join(strArgs, " "))
-			return Nil, nil
+		"help": {
+			Name: "help",
+			Fn: func(args []Sexpr) (Sexpr, error) {
+				doHelp()
+				return Nil, nil
+			},
 		},
-	},
-	"len": {
-		Name: "len",
-		Fn: func(args []Sexpr) (Sexpr, error) {
-			if len(args) != 1 {
-				return nil, fmt.Errorf("len expects a single argument")
-			}
-			list, ok := args[0].(*ConsCell)
-			if !ok {
-				return nil, fmt.Errorf("'%s' is not a list", args[0])
-			}
-			count := 0
-			for list != nil {
-				count++
-				list = list.cdr.(*ConsCell)
-			}
-			return Num(count), nil
-		},
-	},
-	"split": {
-		Name: "split",
-		Fn: func(args []Sexpr) (Sexpr, error) {
-			if len(args) != 1 {
-				return nil, fmt.Errorf("split expects a single argument")
-			}
-			switch s := args[0].(type) {
-			case Atom:
-				return listOfChars(s.String()), nil
-			case Number:
-				return listOfNums(s.String()), nil
-			default:
-				return nil, fmt.Errorf("split expects an atom or a number")
-			}
-		},
-	},
-	"apply": {
-		Name: "apply",
-		Fn: func(args []Sexpr) (Sexpr, error) {
-			if len(args) != 2 {
-				return nil, fmt.Errorf("apply expects exactly two arguments")
-			}
-			fnArgs := []Sexpr{}
-			start := args[1]
-			for start != Nil {
-				cons, ok := start.(*ConsCell)
+		"len": {
+			Name: "len",
+			Fn: func(args []Sexpr) (Sexpr, error) {
+				if len(args) != 1 {
+					return nil, fmt.Errorf("len expects a single argument")
+				}
+				list, ok := args[0].(*ConsCell)
 				if !ok {
-					return nil, fmt.Errorf("'%s' is not a list", start)
+					return nil, fmt.Errorf("'%s' is not a list", args[0])
 				}
-				fnArgs = append(fnArgs, cons.car)
-				start = cons.cdr
-			}
-			return applyFn(args[0], fnArgs)
+				count := 0
+				for list != nil {
+					count++
+					list = list.cdr.(*ConsCell)
+				}
+				return Num(count), nil
+			},
 		},
-	},
+		"split": {
+			Name: "split",
+			Fn: func(args []Sexpr) (Sexpr, error) {
+				if len(args) != 1 {
+					return nil, fmt.Errorf("split expects a single argument")
+				}
+				switch s := args[0].(type) {
+				case Atom:
+					return listOfChars(s.String()), nil
+				case Number:
+					return listOfNums(s.String()), nil
+				default:
+					return nil, fmt.Errorf("split expects an atom or a number")
+				}
+			},
+		},
+		"apply": {
+			Name: "apply",
+			Fn: func(args []Sexpr) (Sexpr, error) {
+				if len(args) != 2 {
+					return nil, fmt.Errorf("apply expects exactly two arguments")
+				}
+				fnArgs := []Sexpr{}
+				start := args[1]
+				for start != Nil {
+					cons, ok := start.(*ConsCell)
+					if !ok {
+						return nil, fmt.Errorf("'%s' is not a list", start)
+					}
+					fnArgs = append(fnArgs, cons.car)
+					start = cons.cdr
+				}
+				return applyFn(args[0], fnArgs)
+			},
+		},
+	}
 }
 
 // listOfChars returns a list of single-character atoms from another, presumably
